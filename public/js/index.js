20834ec6d7bdaa3a4023ae2dc7186dda6e075900
iuriifaevskii <urfaevskii@gmail.com>
2017-11-10 14:13:42 +0200

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
 socket.on('connect', function () {
     console.log('connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Ok, that works for me',
-    });
+    
 });
 socket.on('disconnect', function () {
     console.log('disconnected from server');
